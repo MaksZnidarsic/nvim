3025fe7e45b0d015ad525723ba0f73436dd7c041
Maks <maks.znidarsic@gmail.com>
2026-08-25 17:49:02 +0200

    changed lsp settings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,10 +4,41 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', {})
 
-vim.keymap.set('', '<C-k>', '{', {})
-vim.keymap.set('', '<C-j>', '}', {})
+vim.keymap.set('t', '<esc>', '<c-\\><c-n>', {})
 
-vim.keymap.set('n', '<C-p>', '\"+p', {})
-vim.keymap.set('v', '<C-y>', '\"+y', {})
+vim.keymap.set('', '<c-k>', '{', {})
+vim.keymap.set('', '<c-j>', '}', {})
+
+vim.keymap.set('n', '<c-p>', '\"+p', {})
+vim.keymap.set('v', '<c-y>', '\"+y', {})
+
+
+-- lsp --
+
+local opts = { silent = true }
+
+
+
+-- currently not working
+--vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts) -- smart rename
+
+vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, opts) -- show docs
+-- haven't decided on keybinds
+--vim.keymap.set('n', '<leader>', vim.lsp.buf.definition, opts)
+--vim.keymap.set('n', '<leader>', vim.lsp.buf.declaration, opts)
+
+
+
+vim.keymap.set('n', '<leader>D', '<cmd>Telescope diagnostics<cr><esc>', opts)
+vim.keymap.set('n', '<leader>d', function()
+  vim.diagnostic.config({ virtual_lines = { current_line = true }, virtual_text = false })
+
+  vim.api.nvim_create_autocmd('CursorMoved', {
+    group = vim.api.nvim_create_augroup('line-diagnostics', { clear = true }),
+    callback = function()
+      vim.diagnostic.config({ virtual_lines = false, virtual_text = false })
+      return true
+    end,
+  })
+end)
--- plugin/lspconfig.lua
+++
@@ -1,7 +0,0 @@
-
-
-
-vim.pack.add({
-    'https://github.com/neovim/nvim-lspconfig',
-    'https://github.com/folke/lazydev.nvim'
-})
